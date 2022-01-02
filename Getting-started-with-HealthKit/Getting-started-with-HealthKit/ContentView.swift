ad00dc895ebcbadf6a63ae7e0f61cefffe8d7dc0
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2022-01-01 19:15:38 -0700

    initial commit (blank, hello world xcode project)

---
+++ Getting-started-with-HealthKit/Getting-started-with-HealthKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Getting-started-with-HealthKit
+//
+//  Created by Jonathan Lifferth on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Getting-started-with-HealthKit/Getting-started-with-HealthKit/Getting_started_with_HealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getting_started_with_HealthKitApp.swift
+//  Getting-started-with-HealthKit
+//
+//  Created by Jonathan Lifferth on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Getting_started_with_HealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
